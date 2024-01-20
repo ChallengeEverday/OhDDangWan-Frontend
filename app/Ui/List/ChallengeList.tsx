ea9d9e88c1fbcecdd5b1e39b7c0c58b704d037ee
kheeyaa <kheeyaa@icloud.com>
2024-01-20 23:40:19 +0900

    :sparkles: 챌린지에 태그 형식으로 디테일 노출하기

--- app/Ui/List/ChallengeList.tsx
+++ app/Ui/List/ChallengeList.tsx
@@ -17,7 +17,7 @@ const challengeList = [
     members: 120,
     image: "/running.avif",
     id: 1,
-    tag: ["매일", "1년동안"],
+    details: ["매일", "1년동안"],
   },
   {
     title: "뉴진스 춤 연습하자!",
@@ -26,7 +26,12 @@ const challengeList = [
     members: 12,
     image: "/hani.jpeg",
     id: 2,
-    tag: ["매일", "1년동안"],
+    details: [
+      "하니야하니야하니야1",
+      "하니야하니야하니야2",
+      "하니야하니야하니야3",
+      "사랑해",
+    ],
   },
   {
     title: "알고리즘 인증해요!",
@@ -36,7 +41,7 @@ const challengeList = [
     members: 120,
     image: "/hani.jpeg",
     id: 3,
-    tag: ["매일", "1년동안"],
+    details: ["매일", "1년동안"],
   },
   {
     title: "오운완 챌린지",
@@ -45,45 +50,54 @@ const challengeList = [
     members: 120,
     image: "/running.avif",
     id: 4,
-    tag: ["매일", "1년동안"],
+    details: ["매일", "1년동안"],
   },
 ]
 
 export default function ChallengeList() {
   return (
     <div className="gap-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 mt-10">
-      {challengeList.map(({ title, time, description, members, image, id }) => (
-        <Card key={id} className="pb-4">
-          <Image
-            aria-hidden
-            alt={title}
-            className="aspect-[5/3] object-cover rounded-b-none"
-            src={image}
-          />
-          <CardHeader className="reletive overflow-visible py-0">
-            <Chip
-              startContent={<IoMdPerson size="16" />}
-              className="flex absolute top-3 right-2 z-20"
-              color="primary"
-            >
-              {members}
-            </Chip>
-          </CardHeader>
-          <CardBody className="pb-0 pt-2 px-4 flex-col items-start">
-            <div className="w-full pb-3">
-              <div className="w-full flex justify-between items-center">
-                <h4 className="font-bold text-large">{title}</h4>
-                <p className="text-tiny text-default-500">{time}</p>
+      {challengeList.map(
+        ({ title, time, description, members, image, id, details }) => (
+          <Card key={id} className="pb-4">
+            <Image
+              aria-hidden
+              alt={title}
+              className="aspect-[5/3] object-cover rounded-b-none"
+              src={image}
+            />
+            <CardHeader className="reletive overflow-visible py-0">
+              <Chip
+                startContent={<IoMdPerson size="16" />}
+                className="flex absolute top-3 right-2 z-20"
+                color="primary"
+              >
+                {members}
+              </Chip>
+            </CardHeader>
+            <CardBody className="pb-0 pt-2 px-4 flex-col items-start">
+              <div className="w-full pb-3">
+                <div className="w-full flex justify-between items-center">
+                  <h4 className="font-bold text-large">{title}</h4>
+                  <p className="text-tiny text-default-500">{time}</p>
+                </div>
               </div>
-              {/* <small className="text-default-500">{time}</small> */}
-            </div>
-            <Divider />
-            <div className="w-full pt-3 truncate">
-              <p className="truncate">{description}</p>
-            </div>
-          </CardBody>
-        </Card>
-      ))}
+              <Divider />
+              <div className="w-full pt-3 truncate">
+                <p className="truncate">{description}</p>
+              </div>
+              <div className="w-full select-none flex overflow-x-scroll pt-3">
+                {details &&
+                  details.map((detail) => (
+                    <Chip key={detail} variant="bordered" className="mr-1">
+                      #{detail}
+                    </Chip>
+                  ))}
+              </div>
+            </CardBody>
+          </Card>
+        ),
+      )}
     </div>
   )
 }
