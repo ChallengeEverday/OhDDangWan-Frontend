1d70873e842b885987959c0bd1cf90fb35b3926c
kheeyaa <kheeyaa@icloud.com>
2024-01-20 14:59:20 +0900

    :sparkles: GNB 작업

---
+++ app/Ui/Layout/GNB.tsx
@@ -0,0 +1,88 @@
+"use client"
+
+import { useState } from "react"
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarMenuToggle,
+  NavbarItem,
+  NavbarMenu,
+  NavbarMenuItem,
+  Link,
+  Button,
+} from "@nextui-org/react"
+import { usePathname } from "next/navigation"
+import { gnblinks } from "@/app/data/links"
+
+export default function App() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+  const pathname = usePathname()
+
+  return (
+    <Navbar isBordered isMenuOpen={isMenuOpen} onMenuOpenChange={setIsMenuOpen}>
+      {/* 모바일 UI */}
+      <NavbarContent className="sm:hidden" justify="start">
+        <NavbarMenuToggle
+          aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+        />
+      </NavbarContent>
+
+      <NavbarContent className="sm:hidden pr-3" justify="center">
+        <NavbarBrand>
+          <p className="font-bold text-inherit">💪🏻오땡완</p>
+        </NavbarBrand>
+      </NavbarContent>
+
+      <NavbarMenu>
+        {gnblinks.map(({ title, href }, index) => {
+          const isActive = pathname === href
+          return (
+            <NavbarMenuItem key={title} isActive={isActive}>
+              <Link
+                color={isActive ? "primary" : "foreground"}
+                href={href}
+                size="lg"
+              >
+                {title}
+              </Link>
+            </NavbarMenuItem>
+          )
+        })}
+      </NavbarMenu>
+
+      {/* 데스크탑 UI */}
+      <NavbarContent className="hidden sm:flex">
+        <NavbarBrand>
+          <p className="font-bold text-inherit">💪🏻오땡완</p>
+        </NavbarBrand>
+      </NavbarContent>
+
+      <NavbarContent className="hidden sm:flex gap-4" justify="center">
+        {gnblinks.map(({ title, href }) => {
+          const isActive = pathname === href
+          return (
+            <NavbarItem key={title} isActive={isActive}>
+              <Link
+                color={isActive ? "primary" : "foreground"}
+                href={href}
+                size="lg"
+              >
+                {title}
+              </Link>
+            </NavbarItem>
+          )
+        })}
+      </NavbarContent>
+
+      {/* 공통 UI */}
+      <NavbarContent justify="end">
+        <NavbarItem>
+          <Button as={Link} color="primary" href="/login" variant="flat">
+            로그인
+          </Button>
+        </NavbarItem>
+      </NavbarContent>
+    </Navbar>
+  )
+}
---
+++ app/data/links.ts
@@ -0,0 +1,14 @@
+export const gnblinks = [
+  {
+    title: "✨새로운 챌린지✨",
+    href: "/news",
+  },
+  {
+    title: "🔥Hot한 챌린지🔥",
+    href: "/",
+  },
+  {
+    title: "나의 챌린지",
+    href: "/my",
+  },
+]
