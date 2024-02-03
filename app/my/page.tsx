870cf5a1b19992415c3822d5e8e4702c4b769b5d
kheeyaa <kheeyaa@icloud.com>
2024-02-04 00:30:05 +0900

    :sparkles: #10 마이페이지

--- app/my/page.tsx
+++ app/my/page.tsx
@@ -3,6 +3,7 @@
 import { useRouter } from "next/navigation"
 import { useUserInfoStore } from "../utils/store/userInfoStore"
 import { Avatar, Divider } from "@nextui-org/react"
+import ChallengeList from "../ui/List/ChallengeList"
 
 export default function MyPage() {
   const router = useRouter()
@@ -17,9 +18,9 @@ export default function MyPage() {
   const { nickname, profileImageUrl } = userInfo
 
   return (
-    <main className="w-full my-5 md:my-10">
+    <main className="w-full">
       <h1 className="font-bold text-xl md:text-2xl">마이페이지</h1>
-      <Divider className="my-4" />
+      <Divider className="my-3 md:my-6" />
 
       <section className="flex justify-start items-center gap-4">
         <Avatar
@@ -29,6 +30,18 @@ export default function MyPage() {
           className="md:w-20 md:h-20"
         />
         <h2 className="font-bold md:text-lg">{nickname}</h2>
+      </section>
+
+      <section className="w-full my-4 bg-red-50 h-96">캘린더</section>
+
+      <Divider className="my-6 md:my-8" />
+
+      <section>
+        <h1 className="font-bold text-lg md:text-xl mb-4 md:mb-8">
+          나의 챌린지
+        </h1>
+
+        <ChallengeList />
       </section>
     </main>
   )
