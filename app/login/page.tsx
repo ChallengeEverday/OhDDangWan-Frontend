43b9eea45afdff03890fae003091be83b905e52b
kheeyaa <kheeyaa@icloud.com>
2024-02-04 00:14:57 +0900

    :lipstick: #9 로그인페이지 스타일

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,11 +1,13 @@
 "use client"
 
+import { Divider } from "@nextui-org/react"
 import KakaoLogin from "./ui/KakaoLogin"
 
 export default function Login() {
   return (
-    <main>
-      <h1>오땡완을 시작해보세요!</h1>
+    <main className="w-full my-5 md:my-10">
+      <h1 className="font-bold text-xl md:text-2xl">오땡완을 시작해보세요!</h1>
+      <Divider className="my-4" />
       <KakaoLogin />
     </main>
   )
