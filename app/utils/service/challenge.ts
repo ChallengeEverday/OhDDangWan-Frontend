6afe528c218510b0abaae5bff3f2d432ea8eae59
kheeyaa <kheeyaa@icloud.com>
2024-02-11 20:41:14 +0900

    :sparkles: #14 챌린지 post, get api

---
+++ app/utils/service/challenge.ts
@@ -0,0 +1,26 @@
+import { POST_ChallengeForm } from "../types/challenge"
+import api from "./api"
+
+export const get_challenges = async (id: string) => {}
+
+export const get_challenge_$challengeId = async (id: string) => {
+  try {
+    const result = await api.get(`/v1/challenges/${id}`)
+    return result
+  } catch (e) {
+    console.error(e)
+    throw new Error(
+      `챌린지를 불러오는데 실패하였습니다. get /v1/challenges/${id}`,
+    )
+  }
+}
+
+export const post_challenges = async (challengeForm: POST_ChallengeForm) => {
+  try {
+    const result = await api.post("/v1/challenges", challengeForm)
+    return result
+  } catch (e) {
+    console.error(e)
+    throw new Error("챌린지를 생성하는데 실패하였습니다. post /v1/challenges")
+  }
+}
