59c60fdb13da0ebbf9f92364e476982591c7c70b
kheeyaa <kheeyaa@icloud.com>
2024-02-25 19:40:05 +0900

    :bug: 403 케이스 대응

--- app/utils/service/api.ts
+++ app/utils/service/api.ts
@@ -1,7 +1,22 @@
 import axios from "axios"
+import { useUserInfoStore } from "../store/userInfoStore"
 
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API,
   withCredentials: true,
 })
+
+api.interceptors.response.use(
+  (response) => {
+    return response
+  },
+  (error) => {
+    if (error.response.status === 403) {
+      window.location.href = "/login"
+      useUserInfoStore.getState().removeUserInfo()
+    }
+    return Promise.reject(error)
+  },
+)
+
 export default api
