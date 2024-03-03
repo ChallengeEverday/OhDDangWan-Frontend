7efe2a38029b867f30f5652f3d80ae7887ee7533
kheeyaa <kheeyaa@icloud.com>
2024-03-03 19:26:16 +0900

    queryClient utils로 분리

--- app/providers.tsx
+++ app/providers.tsx
@@ -3,9 +3,9 @@
 import { NextUIProvider } from "@nextui-org/react"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
 import usePrefersDarkMode from "./utils/hooks/usePrefersDarkMode"
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { QueryClientProvider } from "@tanstack/react-query"
 import { useInitUser } from "./utils/hooks/useInitUser"
-const queryClient = new QueryClient()
+import { queryClient } from "./utils/service/query"
 
 export function Providers({ children }: { children: React.ReactNode }) {
   const prefersDarkMode = usePrefersDarkMode()
