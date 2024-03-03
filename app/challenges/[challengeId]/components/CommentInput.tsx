03ee7b2e4363ce9f3fbe037fc7b5f99bdd04674c
kheeyaa <kheeyaa@icloud.com>
2024-03-03 19:26:44 +0900

    #19 댓글 작성 로직 개선

--- app/challenges/[challengeId]/components/CommentInput.tsx
+++ app/challenges/[challengeId]/components/CommentInput.tsx
@@ -1,6 +1,9 @@
 "use client"
 
-import { post_comments_$challengeId } from "@/app/utils/service/comments"
+import {
+  post_comments_$challengeId,
+  refetch_comments_$challengeId,
+} from "@/app/utils/service/comments"
 import { useUserInfoStore } from "@/app/utils/store/userInfoStore"
 import { Button, Textarea } from "@nextui-org/react"
 import { useParams } from "next/navigation"
@@ -12,9 +15,10 @@ export function CommentInput() {
   const [comment, setComment] = useState("")
 
   const postComment = async (content: string) => {
+    if (content.trim() === "") return
     try {
       await post_comments_$challengeId(params.challengeId as string, {
-        content,
+        content: content.trim(),
       })
     } catch (e) {
       console.error(e)
@@ -22,47 +26,51 @@ export function CommentInput() {
     }
   }
 
-  if (userInfo) {
-    const { nickname } = userInfo
+  if (!userInfo)
     return (
-      <div className="w-full flex flex-col justify-end gap-2">
+      <div className="w-full flex gap-2">
         <Textarea
           variant="bordered"
           labelPlacement="outside"
-          placeholder={`${nickname}(으)로 댓글 달기...`}
+          placeholder={"로그인 후 댓글을 달아주세요."}
+          readOnly
           className="w-full"
-          content={comment}
-          onChange={(e) => setComment(e.target.value)}
         />
-        <div className="flex justify-end">
-          <Button
-            onClick={async () => {
-              await postComment(comment)
-              setComment("")
-            }}
-            size="lg"
-            className="grow-0"
-            variant="solid"
-          >
-            등록
-          </Button>
-        </div>
+        <Button isDisabled variant="solid">
+          등록
+        </Button>
       </div>
     )
-  }
 
+  const { nickname } = userInfo
   return (
-    <div className="w-full flex gap-2">
+    <div className="w-full flex flex-col justify-end gap-2">
       <Textarea
         variant="bordered"
         labelPlacement="outside"
-        placeholder={"로그인 후 댓글을 달아주세요."}
-        readOnly
+        placeholder={`${nickname}(으)로 댓글 달기...`}
         className="w-full"
+        value={comment}
+        onChange={(e) => {
+          console.log("onChange", e.target.value)
+          setComment(e.target.value)
+        }}
       />
-      <Button isDisabled variant="solid">
-        등록
-      </Button>
+      <div className="flex justify-end">
+        <Button
+          onClick={async (e) => {
+            e.preventDefault()
+            await postComment(comment)
+            await refetch_comments_$challengeId(params.challengeId as string)
+            setComment("")
+          }}
+          size="lg"
+          className="grow-0"
+          variant="solid"
+        >
+          등록
+        </Button>
+      </div>
     </div>
   )
 }
